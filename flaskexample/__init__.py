3978e45bcb401ca68f51f0d91362e9b59dc67ba3
Rong <rongma.10@gmail.com>
2019-09-19 17:37:14 -0400

    updated htmls and structure. now working landing page and uploading csv

--- flaskexample/__init__.py
+++ flaskexample/__init__.py
@@ -1,3 +1,12 @@
 from flask import Flask
+import os
+
+
+ALLOWED_EXTENSIONS = set(['csv'])
+UPLOAD_FOLDER = './flaskexample/uploads'
+cwd = os.getcwd()
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 from flaskexample import views
